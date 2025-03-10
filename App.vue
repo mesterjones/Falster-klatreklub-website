8de6f5e7dd46ecd6fe6d89d0968436e284d5189c
Riis <jonas.riis@scandlines.com>
2025-03-10 19:35:01 +0100

    Google Tag tilføjet og mindre baggrundsbillede

--- App.vue
+++ App.vue
@@ -6,4 +6,23 @@
 
 <script setup>
 // No need to import NavBar here as it will be used in the default layout
+
+// Google Analytics setup
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-NGNWM2QN6M',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-NGNWM2QN6M');
+      `,
+      type: 'text/javascript'
+    }
+  ]
+})
 </script>
